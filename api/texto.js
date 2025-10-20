8a93c94c83be00778b36f1abac2d678e47a02abb
embarr <dexfbackmaque@gmail.com>
2025-10-20 15:14:23 -0300

    Delete texto.js

--- api/texto.js
+++
@@ -1,9 +0,0 @@
-import Jimp from 'jimp';
-
-export async function addText(text) {
-  const image = new Jimp(600, 300, "#FFFFFF");
-  const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-  image.print(font, 10, 10, text);
-  const buffer = await image.getBufferAsync(Jimp.MIME_PNG);
-  return buffer;
-}
